94738237b47e684f8f3800868411f63c2d73ff1d
Wasiur Rahman <wrahman@uber.com>
2020-07-18 21:56:10 -0700

    Adding to the scaffolding

---
+++ src/components/Feed.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../stylesheets/Feed.scss';
+
+class Feed extends Component {
+    render() {
+        return (
+            <div className="Feed">
+
+            </div>
+        );
+    }
+}
+
+export default Feed;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../stylesheets/Header.scss';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+            </div>
+        );
+    }
+}
+
+export default Header;
