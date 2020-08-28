577ed892443b5c9de66f5a18b4a35f0e89bafd0f
srahman1000 <siam_1000@hotmail.com>
2020-08-27 22:23:18 -0400

    reviews work with new modal update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import '../stylesheets/Modal.scss';
 
+const ratingImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Green_star.svg/1024px-Green_star.svg.png";
+const ratingImageGray = "https://purepng.com/public/uploads/large/purepng.com-grey-starstargeometricallydecagonconcavestardomclipartblackgrey-1421526502793oblca.png";
+
 class Modal extends Component {
     constructor(props) {
         super(props);
@@ -24,11 +27,27 @@ class Modal extends Component {
     }
 
     render() {
+        this.reviews = this.state.reviews.map((item) => {
+            console.log(item);
+            return <div>
+                <ol className="review-list">{item.review} - {item.rating}</ol>
+                {
+                    Array(5).fill(0).map((val, i) => {
+                        if (i < Math.floor(item.rating)) {
+                            return <img className="ratingImage" src={ratingImage} alt='Rating Image' key={i}/>
+                        } else{
+                            return <img className="ratingImage-gray" src={ratingImageGray} alt='Rating Image Gray' key={i}/>
+                        }
+                    })
+                }
+            </div>
+        })
         return (
             <div className={`${this.state.showModal ? "modal" : "modal-hidden"}`}>
                 <div className="content">
                     <span className="close-button" onClick={this.hideModalToggle}>&times;</span>
                     <p>This person has {this.state.reviews.length} review(s) to show</p>
+                    <p className="review-list2">{this.reviews}</p>
                 </div>
             </div>
         );
